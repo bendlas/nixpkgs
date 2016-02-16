874010bed658aa643d169878eb72e3ffa31af18f
Herwig Hochleitner <herwig@bendlas.net>
2016-02-16 03:49:10 +0100

    [WIP][bendlas] depot-tools

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9370,6 +9370,8 @@ in
 
   gn = callPackage ../development/tools/build-managers/gn { };
 
+  depot_tools = callPackage ../development/tools/build-managers/depot-tools { };
+
   nixbang = callPackage ../development/tools/misc/nixbang {
     pythonPackages = python3Packages;
   };
