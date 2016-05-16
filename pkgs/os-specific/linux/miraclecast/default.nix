fcbe2e376b343347ec2115f43a28283de4e92591
Herwig Hochleitner <herwig@bendlas.net>
2016-05-16 02:41:49 +0200

    miraclecast: update and provide tools

--- pkgs/os-specific/linux/miraclecast/default.nix
+++ pkgs/os-specific/linux/miraclecast/default.nix
@@ -1,15 +1,30 @@
-{ stdenv, fetchFromGitHub, autoreconfHook, pkgconfig, udev, systemd, glib, readline }:
+{ stdenv, fetchFromGitHub, autoreconfHook, pkgconfig, udev, systemd, glib, readline,
+  iproute, gstreamer }:
 
 with stdenv.lib;
 stdenv.mkDerivation rec {
-  name = "miraclecast-0.0-git-20151002";
+  name = "miraclecast-1.0-git-20160402";
 
   src = fetchFromGitHub {
     owner = "albfan";
     repo = "miraclecast";
-    rev = "30b8c2d22391423f76ba582aaaa1e0936869103a";
-    sha256 = "0i076n76kq64fayc7v06gr1853pk5r6ms86m57vd1xsjd0r9wyxd";
+    rev = "c94be167c85c6ec8badd7ac79e3dea2e0b73225c";
+    sha256 = "1hjga7z6sl9albk4iprcv4nx8r1xkm5cp4c034g3g8d3a4yx45lf";
   };
+
+  patches = ./0001-nix-path-configuration.patch;
+
+  IP = "${iproute}/bin/ip";
+  GST_LAUNCH = "${gstreamer}/bin/gst-launch";
+  postPatch = ''
+    substituteInPlace res/miracle-gst.sh --subst-var GST_LAUNCH
+    substituteInPlace src/dhcp/dhcp.c --subst-var IP
+  '';
+  postInstall = ''
+    mkdir -p $out/share/miraclecast $out/etc/dbus-1/system.d
+    cp res/*.sh $out/share/miraclecast
+    cp res/org.freedesktop.miracle.conf $out/etc/dbus-1/system.d/
+  '';
 
   # INFO: It is important to list 'systemd' first as for now miraclecast
   # links against a customized systemd. Otherwise, a systemd package from
