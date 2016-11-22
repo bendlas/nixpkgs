49803a2b7ce08a20968ae07e21196910c990e24a
Herwig Hochleitner <herwig@bendlas.net>
2016-11-22 17:04:38 +0100

    db62: fix

--- pkgs/development/libraries/db/db-6.2.nix
+++ pkgs/development/libraries/db/db-6.2.nix
@@ -4,5 +4,4 @@ import ./generic.nix (args // rec {
   version = "6.2.23";
   sha256 = "1isxx4jfmnh913jzhp8hhfngbk6dsg46f4kjpvvc56maj64jqqa7";
   license = stdenv.lib.licenses.agpl3;
-  branch = "6.2";
 })
