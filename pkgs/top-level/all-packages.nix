1ef41b50e9d0feac77c8a391bc00c0cf5b532103
Herwig Hochleitner <herwig@bendlas.net>
2018-12-04 02:51:35 +0100

    [WIP] planck

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3940,6 +3940,8 @@ with pkgs;
     inherit (darwin) DarwinTools;
   };
 
+  planck = callPackage ../development/interpreters/clojurescript/planck { };
+
   platformio = if stdenv.hostPlatform.isLinux then platformio-chrootenv else platformio-core;
 
   playbar2 = libsForQt5.callPackage ../applications/audio/playbar2 { };
