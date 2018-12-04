c95a288e6858a9b008ec629db18238f5b3f489ff
Herwig Hochleitner <herwig@bendlas.net>
2018-12-04 02:51:35 +0100

    [WIP] planck

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4174,6 +4174,8 @@ with pkgs;
     inherit (darwin) DarwinTools;
   };
 
+  planck = callPackage ../development/interpreters/clojurescript/planck { };
+
   platformioPackages = dontRecurseIntoAttrs (callPackage ../development/embedded/platformio { });
   platformio =
     if stdenv.hostPlatform.isLinux then
