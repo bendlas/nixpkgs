0658c6aba780a552f1d938afee3873cf000f88e9
Herwig Hochleitner <herwig@bendlas.net>
2018-12-04 02:51:35 +0100

    [WIP] planck

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5530,6 +5530,8 @@ in
     graphviz = graphviz_2_32;
   };
 
+  planck = callPackage ../development/interpreters/clojurescript/planck { };
+
   plan9port = callPackage ../tools/system/plan9port { };
 
   platformioPackages = dontRecurseIntoAttrs (callPackage ../development/arduino/platformio { });
