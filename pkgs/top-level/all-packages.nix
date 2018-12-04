0782a0f9af4ee9db1cde1e6fc9a1f6ba96ea3324
Herwig Hochleitner <herwig@bendlas.net>
2018-12-04 02:51:35 +0100

    [WIP] planck

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3160,6 +3160,8 @@ with pkgs;
     inherit (darwin) DarwinTools;
   };
 
+  planck = callPackage ../development/interpreters/clojurescript/planck { };
+
   platformio = if stdenv.hostPlatform.isLinux then platformio-chrootenv else platformio-core;
 
   playwright = playwright-driver;
