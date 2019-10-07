ad36aeaf41792603d41c2761da23b5c4927018ae
Herwig Hochleitner <herwig@bendlas.net>
2019-10-07 15:49:31 +0200

    cinelerra: 2016-01-12 -> 2018-05-16

--- pkgs/applications/video/cinelerra/default.nix
+++ pkgs/applications/video/cinelerra/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchgit, autoconf, automake, libtool
+{ stdenv, fetchFromGitHub, autoconf, automake, libtool
 , pkgconfig, faad2, faac, a52dec, alsaLib, fftw, lame, libavc1394
 , libiec61883, libraw1394, libsndfile, libvorbis, libogg, libjpeg
 , libtiff, freetype, mjpegtools, x264, gettext, openexr
@@ -7,12 +7,13 @@
 , fontconfig, intltool }:
 
 stdenv.mkDerivation {
-  name = "cinelerra-unstable-2016-01-12";
+  name = "cinelerra-unstable-2018-05-16";
 
-  src = fetchgit {
-    url = "git://git.cinelerra-cv.org/j6t/cinelerra.git";
-    rev = "454be60e201c18c1fc3f1f253a6d2184fcfc94c4";
-    sha256 = "1n4kshqhgnr7aivsi8dgx48phyd2nzvv4szbc82mndklvs9jfb7r";
+  src = fetchFromGitHub {
+    owner = "ratopi";
+    repo = "CinelerraCV";
+    rev = "d9c0dbf4393717f0a42f4b91c3e1ed5b16f955dc";
+    sha256 = "0a8kfm1v96sv6jh4568crg6nkr6n3579i9xksfj8w199s6yxzsbk";
   };
 
   # touch config.rpath: work around bug in automake 1.10 ?
