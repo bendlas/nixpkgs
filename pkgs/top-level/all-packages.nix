20eb1150a7db8069d485d68e27f4b99c0d44392f
Herwig Hochleitner <herwig@bendlas.net>
2021-05-23 20:29:53 +0200

    [WIP] hackrf-spektrum-analyzer

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31460,6 +31460,8 @@ with pkgs;
 
   hackrf = callPackage ../applications/radio/hackrf { };
 
+  hackrf-spectrum-analyser = callPackage ../applications/radio/hackrf-spectrum-analyzer { };
+
   hacksaw = callPackage ../tools/misc/hacksaw { };
 
   hakuneko = callPackage ../tools/misc/hakuneko { };
