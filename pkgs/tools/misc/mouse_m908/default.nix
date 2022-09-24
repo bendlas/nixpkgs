fd11affbdd3e9fa56aa4aaad521e97ccd6cc41e3
Herwig Hochleitner <herwig@bendlas.net>
2022-09-24 13:34:43 +0200

    mouse_m908: expermental version

--- pkgs/tools/misc/mouse_m908/default.nix
+++ pkgs/tools/misc/mouse_m908/default.nix
@@ -5,11 +5,12 @@
 stdenv.mkDerivation rec {
 
   pname = "mouse_m908";
-  version = "3.3";
+  version = "3.4-pre";
   src = fetchFromGitHub {
     owner = "dokutan";
     repo = pname;
-    rev = "v${version}";
+    # rev = "v${version}";
+    rev = "6f4e9b5c9fdfc43cd241a0a3d8f9e1736a9588bf";
     sha256 = "sha256-jat5K7P3Z5R1L5jI4pilNFU7q0xtRxwd4oXMYRW0mUU=";
   };
 
