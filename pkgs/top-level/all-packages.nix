8aad0c1eecc391bf95e54d80530e21c055cb638d
Herwig Hochleitner <herwig@bendlas.net>
2022-12-18 23:10:28 +0100

    [WIP] brscan3 and mfc6490 drivers

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -39014,6 +39014,9 @@ with pkgs;
   mfc5890cncupswrapper = callPackage ../misc/cups/drivers/mfc5890cncupswrapper { };
   mfc5890cnlpr = callPackage ../misc/cups/drivers/mfc5890cnlpr { };
 
+  mfc6490cwcupswrapper = callPackage ../misc/cups/drivers/mfc6490cwcupswrapper { };
+  mfc6490cwlpr = callPackage ../misc/cups/drivers/mfc6490cwlpr { };
+
   mfc9140cdncupswrapper = callPackage ../misc/cups/drivers/mfc9140cdncupswrapper { };
   mfc9140cdnlpr = callPackage ../misc/cups/drivers/mfc9140cdnlpr { };
 
@@ -39033,6 +39036,8 @@ with pkgs;
   fflinuxprint = callPackage ../misc/cups/drivers/fflinuxprint { };
 
   fxlinuxprint = callPackage ../misc/cups/drivers/fxlinuxprint { };
+
+  brscan3 = callPackage ../applications/graphics/sane/backends/brscan3 { };
 
   brscan4 = callPackage ../applications/graphics/sane/backends/brscan4 { };
 
