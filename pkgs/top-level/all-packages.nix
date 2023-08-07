f9b86a14b0cc4ca1c82e5fa728edc4398cfa8913
Rotox Local <herwig@rotox.local>
2023-08-07 02:41:52 +0200

    v4l2-request-ffmpeg: init 5.1.2
    also add mpv version

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21107,6 +21107,9 @@ with pkgs;
     ffmpegVariant = "full";
   };
 
+  v4l2-request-ffmpeg = callPackage ../development/libraries/v4l2-request-ffmpeg { };
+  v4l2-request-mpv = wrapMpv (mpv-unwrapped.override { ffmpeg = v4l2-request-ffmpeg; }) {};
+
   ffmpeg_6 = callPackage ../development/libraries/ffmpeg/6.nix {
      inherit (darwin.apple_sdk.frameworks)
       Cocoa CoreServices CoreAudio CoreMedia AVFoundation MediaToolbox
