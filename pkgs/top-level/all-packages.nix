1efa742f72f334b0a476243e27ea87e961fdb501
Rotox Local <herwig@rotox.local>
2023-08-07 02:41:52 +0200

    v4l2-request-ffmpeg: init 5.1.2
    also add mpv version

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20109,6 +20109,9 @@ with pkgs;
     ffmpeg-headless
     ffmpeg-full;
 
+  v4l2-request-ffmpeg = callPackage ../development/libraries/v4l2-request-ffmpeg { };
+  v4l2-request-mpv = wrapMpv (mpv-unwrapped.override { ffmpeg = v4l2-request-ffmpeg; }) {};
+
   ffmpegthumbnailer = callPackage ../development/libraries/ffmpegthumbnailer { };
 
   ffmpeg-normalize = python3Packages.callPackage ../applications/video/ffmpeg-normalize { };
