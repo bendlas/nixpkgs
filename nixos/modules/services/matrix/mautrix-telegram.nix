afe62035ebfdebd5fda3ae94e186e07d9f0c1003
Herwig Hochleitner <herwig@bendlas.net>
2024-02-24 01:06:23 +0100

    nixos/mautrix-telegram: fix preStart
    we're not accepting a base config any more

--- nixos/modules/services/matrix/mautrix-telegram.nix
+++ nixos/modules/services/matrix/mautrix-telegram.nix
@@ -170,7 +170,6 @@ in {
           # (re-)generate the appservice's registration file
           ${pkgs.mautrix-telegram}/bin/mautrix-telegram \
             --generate-registration \
-            --base-config='${pkgs.mautrix-telegram}/${pkgs.mautrix-telegram.pythonModule.sitePackages}/mautrix_telegram/example-config.yaml' \
             --config='${settingsFile}' \
             --registration='${registrationFile}'
         fi
