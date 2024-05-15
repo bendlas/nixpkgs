72035dcee42590ad54d3c979115d667a064017de
Herwig Hochleitner <herwig@bendlas.net>
2024-05-16 01:03:28 +0200

    jdk libre bootstrap: get up to ant 1.8.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15582,6 +15582,8 @@ with pkgs;
   jdk = jdk21;
   jdk_headless = jdk21_headless;
 
+  jdkBootstrapLibre = callPackage ../development/compilers/openjdk/bootstrap-libre { };
+
   # Since the introduction of the Java Platform Module System in Java 9, Java
   # no longer ships a separate JRE package.
   #
