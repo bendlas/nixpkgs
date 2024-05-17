38788ff5d7d312e22a2b8bbe6fe419b6556fc204
Herwig Hochleitner <herwig@bendlas.net>
2024-05-17 13:41:26 +0200

    Revert "icedtea: Remove"
    This reverts commit a8d10100279636146b8d66da582e1f23d9d91742.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5588,6 +5588,11 @@ with pkgs;
 
   gwt240 = callPackage ../development/compilers/gwt/2.4.0.nix { };
 
+  icedtea7 = callPackage ../development/compilers/icedtea rec {
+    bootjdk = openjdk-bootstrap;
+    giflib = giflib_5_0;
+  };
+
   idrisPackages = dontRecurseIntoAttrs (
     callPackage ../development/idris-modules {
       idris-no-deps = haskellPackages.idris;
