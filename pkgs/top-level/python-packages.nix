4b444014da573b99a98a63963ea42832097eb7a8
Herwig Hochleitner <herwig@bendlas.net>
2024-06-26 14:14:07 +0200

    pythonPackages.friendly-traceback: init
    needed for grist-core

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4898,6 +4898,8 @@ self: super: with self; {
 
   frida-python = callPackage ../development/python-modules/frida-python { };
 
+  friendly-traceback = callPackage ../development/python-modules/friendly-traceback { };
+
   frigidaire = callPackage ../development/python-modules/frigidaire { };
 
   frilouz = callPackage ../development/python-modules/frilouz { };
