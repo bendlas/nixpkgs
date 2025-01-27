9404a3475bb7ad357f946ff4568fac36ece0ec8e
Scandiravian <nixos@scandiravian.com>
2025-01-27 11:20:44 +0100

    nixos/grist-core: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1505,6 +1505,7 @@
   ./services/web-apps/gotify-server.nix
   ./services/web-apps/gotosocial.nix
   ./services/web-apps/grav.nix
+  ./services/web-apps/grist-core.nix
   ./services/web-apps/grocy.nix
   ./services/web-apps/pixelfed.nix
   ./services/web-apps/goatcounter.nix
