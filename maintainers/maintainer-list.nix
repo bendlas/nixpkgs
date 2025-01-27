e768233bae14c1c75a191ddaab639361709bacb2
Scandiravian <nixos@scandiravian.com>
2025-01-27 11:17:00 +0100

    grist-core: init at 1.3.2

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -21867,6 +21867,12 @@
     githubId = 3958212;
     name = "Tom Sorlie";
   };
+  scandiravian = {
+    email = "nixos@scandiravian.com";
+    github = "scandiravian";
+    githubId = 13556969;
+    name = "Scandiravian";
+  };
   scd31 = {
     name = "scd31";
     github = "scd31";
