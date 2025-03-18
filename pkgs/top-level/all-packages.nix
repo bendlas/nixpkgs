713a6e116e541dd1f8b8f858f40639bff2ef2a98
Herwig Hochleitner <herwig@bendlas.net>
2025-03-18 13:41:30 +0100

    openvscode-server: remove node override

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15489,7 +15489,6 @@ with pkgs;
   };
 
   openvscode-server = callPackage ../servers/openvscode-server {
-    nodejs = nodejs_22;
     inherit (darwin.apple_sdk.frameworks) AppKit Cocoa Security;
   };
 
