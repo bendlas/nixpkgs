cf621c2ae7188d5b3155343207d9978d708d120b
Leif Huhn <lb.github@lisbethmail.com>
2025-03-27 19:55:43 -0400

    Fix vllm. Very preliminary.

--- pkgs/development/python-modules/vllm/default.nix
+++ pkgs/development/python-modules/vllm/default.nix
@@ -72,6 +72,13 @@
   flashinfer,
   py-libnuma,
 
+  awscli,
+  boto3,
+  botocore,
+  peft,
+  pytest-asyncio,
+  #tensorizer,
+
   # internal dependency - for overriding in overlays
   vllm-flash-attn ? null,
 
@@ -93,6 +100,45 @@ let
 
   shouldUsePkg =
     pkg: if pkg != null && lib.meta.availableOn stdenv.hostPlatform pkg then pkg else null;
+
+  rocmtoolkit_joined = symlinkJoin {
+    name = "rocm-merged";
+
+    paths = with rocmPackages; [
+      rocm-core
+      clr
+      rccl
+      miopen
+      aotriton
+      rocrand
+      rocblas
+      rocsparse
+      hipsparse
+      rocthrust
+      rocprim
+      hipcub
+      roctracer
+      rocfft
+      rocsolver
+      hipfft
+      hiprand
+      hipsolver
+      hipblas-common
+      hipblas
+      hipblaslt
+      rocminfo
+      rocm-comgr
+      rocm-device-libs
+      rocm-runtime
+      clr.icd
+      hipify
+    ];
+
+    # Fix `setuptools` not being found
+    postBuild = ''
+      rm -rf $out/nix-support
+    '';
+  };
 
   # see CMakeLists.txt, grepping for GIT_TAG near cutlass
   # https://github.com/vllm-project/vllm/blob/v${version}/CMakeLists.txt
@@ -284,23 +330,50 @@ buildPythonPackage rec {
       --replace-fail "torch ==" "torch >=" \
       --replace-fail "setuptools>=77.0.3,<80.0.0" "setuptools"
 
+    # Pass build environment PYTHONPATH to vLLM's Python configuration scripts
     # Ignore the python version check because it hard-codes minor versions and
     # lags behind `ray`'s python interpreter support
+    # Here I just took some random comment and added the line I want.
+    # This really needs to be a patch.
+    # I have _no_ idea how vllm builds on FHS systems. I don't see how torch
+    # causes vllm to find HIP. Let's just tell cmake to find HIP.
     substituteInPlace CMakeLists.txt \
+      --replace-fail '$PYTHONPATH' '$ENV{PYTHONPATH}' \
       --replace-fail \
         'set(PYTHON_SUPPORTED_VERSIONS' \
-        'set(PYTHON_SUPPORTED_VERSIONS "${lib.versions.majorMinor python.version}"'
+        'set(PYTHON_SUPPORTED_VERSIONS "${lib.versions.majorMinor python.version}"' \
+      --replace-fail \
+        '# Forward the non-CUDA device extensions to external CMake scripts.' \
+        'find_package(HIP REQUIRED)'
 
-    # Pass build environment PYTHONPATH to vLLM's Python configuration scripts
-    substituteInPlace CMakeLists.txt \
-      --replace-fail '$PYTHONPATH' '$ENV{PYTHONPATH}'
+    substituteInPlace csrc/quantization/compressed_tensors/int8_quant_kernels.cu \
+      --replace-fail \
+      'dst = std::clamp(dst, i8_min, i8_max);' \
+      'dst = (dst < i8_min) ? i8_min : (dst > i8_max) ? i8_max : dst;' \
+      --replace-fail \
+      'int32_t dst = std::clamp(x, i8_min, i8_max);' \
+      'int32_t dst = (x < i8_min) ? i8_min : (x > i8_max) ? i8_max : x;'
+
+
+    substituteInPlace csrc/quantization/fused_kernels/quant_conversions.cuh \
+      --replace-fail \
+      'dst = std::clamp(dst, i8_min, i8_max);' \
+      'dst = (dst < i8_min) ? i8_min : (dst > i8_max) ? i8_max : dst;'
+
+    substituteInPlace setup.py \
+      --replace-fail \
+        'and torch.version.hip is not None' \
+        ' ' \
+      --replace-fail \
+        'hipcc_version = get_hipcc_rocm_version()' \
+        'hipcc_version = "0.0"'
   '';
 
   nativeBuildInputs = [
     which
   ]
   ++ lib.optionals rocmSupport [
-    rocmPackages.hipcc
+    rocmtoolkit_joined
   ]
   ++ lib.optionals cudaSupport [
     cudaPackages.cuda_nvcc
@@ -335,16 +408,7 @@ buildPythonPackage rec {
         libcufile
       ])
     )
-    ++ lib.optionals rocmSupport (
-      with rocmPackages;
-      [
-        clr
-        rocthrust
-        rocprim
-        hipsparse
-        hipblas
-      ]
-    )
+    ++ lib.optionals rocmSupport [ rocmtoolkit_joined ]
     ++ lib.optionals stdenv.cc.isClang [
       llvmPackages.openmp
     ];
@@ -364,6 +428,7 @@ buildPythonPackage rec {
     outlines
     pandas
     prometheus-fastapi-instrumentator
+    psutil
     py-cpuinfo
     pyarrow
     pybase64
@@ -396,6 +461,13 @@ buildPythonPackage rec {
     bitsandbytes
     # vLLM needs Torch's compiler to be present in order to use torch.compile
     torch.stdenv.cc
+
+    awscli
+    boto3
+    botocore
+    peft
+    pytest-asyncio
+    #tensorizer
   ]
   ++ uvicorn.optional-dependencies.standard
   ++ aioprometheus.optional-dependencies.starlette
@@ -411,6 +483,13 @@ buildPythonPackage rec {
 
   dontUseCmakeConfigure = true;
   cmakeFlags = [
+  ]
+  ++ lib.optionals rocmSupport [
+    (lib.cmakeFeature "CMAKE_HIP_COMPILER" "${rocmPackages.clr.hipClangPath}/clang++")
+    # TODO: this should become `clr.gpuTargets` in the future.
+    #(lib.cmakeFeature "CMAKE_HIP_ARCHITECTURES" rocmPackages.rocblas.amdgpu_targets)
+    (lib.cmakeFeature "CMAKE_HIP_ARCHITECTURES" "gfx1100")
+    (lib.cmakeFeature "AMDGPU_TARGETS" "gfx1100")
   ]
   ++ lib.optionals cudaSupport [
     (lib.cmakeFeature "FETCHCONTENT_SOURCE_DIR_CUTLASS" "${lib.getDev cutlass}")
@@ -438,8 +517,10 @@ buildPythonPackage rec {
     // lib.optionalAttrs rocmSupport {
       VLLM_TARGET_DEVICE = "rocm";
       # Otherwise it tries to enumerate host supported ROCM gfx archs, and that is not possible due to sandboxing.
-      PYTORCH_ROCM_ARCH = lib.strings.concatStringsSep ";" rocmPackages.clr.gpuTargets;
-      ROCM_HOME = "${rocmPackages.clr}";
+      #PYTORCH_ROCM_ARCH = lib.strings.concatStringsSep ";" rocmPackages.clr.gpuTargets;
+      PYTORCH_ROCM_ARCH = "gfx1100";
+      ROCM_PATH = "${rocmtoolkit_joined}";
+      ROCM_HOME = "${rocmtoolkit_joined}";
     }
     // lib.optionalAttrs cpuSupport {
       VLLM_TARGET_DEVICE = "cpu";
@@ -453,6 +534,8 @@ buildPythonPackage rec {
 
   pythonRelaxDeps = true;
 
+  # The runtime check is checking for tensorizer, which isn't packages, AFAIK.
+  dontCheckRuntimeDeps = true;
   pythonImportsCheck = [ "vllm" ];
 
   passthru = {
