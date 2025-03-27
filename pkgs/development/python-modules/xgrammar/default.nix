3c17aeb77954696907b096807584c7e8b81d55a1
Leif Huhn <lb.github@lisbethmail.com>
2025-03-27 19:55:43 -0400

    Fix vllm. Very preliminary.

--- pkgs/development/python-modules/xgrammar/default.nix
+++ pkgs/development/python-modules/xgrammar/default.nix
@@ -16,7 +16,7 @@
   tiktoken,
   torch,
   transformers,
-  triton,
+  rocmPackages,
 
   # tests
   pytestCheckHook,
@@ -56,7 +56,7 @@ buildPythonPackage rec {
     transformers
   ]
   ++ lib.optionals (stdenv.hostPlatform.isLinux && stdenv.hostPlatform.isx86_64) [
-    triton
+    rocmPackages.triton
   ];
 
   nativeCheckInputs = [
