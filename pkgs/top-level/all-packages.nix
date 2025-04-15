347b2d7ac69776c643496210b8577474932b792f
Joshua Skrzypek <jskrzypek@gmail.com>
2025-04-15 17:22:32 -0400

    void-editor: init at 1.99.30001

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14227,6 +14227,12 @@ with pkgs;
   code-cursor-fhs = code-cursor.fhs;
   code-cursor-fhsWithPackages = code-cursor.fhsWithPackages;
 
+  void-editor = callPackage ../by-name/vo/void-editor/package.nix {
+    vscode-generic = ../applications/editors/vscode/generic.nix;
+  };
+  void-editor-fhs = void-editor.fhs;
+  void-editor-fhsWithPackages = void-editor.fhsWithPackages;
+
   windsurf = callPackage ../by-name/wi/windsurf/package.nix {
     vscode-generic = ../applications/editors/vscode/generic.nix;
   };
