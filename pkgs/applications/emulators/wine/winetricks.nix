354a4ece4dc76666826f5f0a28780ad86cb05247
Herwig Hochleitner <herwig@bendlas.net>
2025-08-05 10:23:28 +0200

    winetricks: apply patch to make WINE_BIN and WINESERVER_BIN overridable

--- pkgs/applications/emulators/wine/winetricks.nix
+++ pkgs/applications/emulators/wine/winetricks.nix
@@ -16,6 +16,7 @@
   gawk,
   gnutar,
   gzip,
+  fetchpatch,
 }:
 
 stdenv.mkDerivation rec {
@@ -23,6 +24,15 @@ stdenv.mkDerivation rec {
   version = src.version;
 
   src = (callPackage ./sources.nix { }).winetricks;
+
+  patches = [
+    (fetchpatch {
+      # make WINE_BIN and WINESERVER_BIN overridable
+      # see https://github.com/NixOS/nixpkgs/issues/338367
+      url = "https://github.com/Winetricks/winetricks/commit/1d441b422d9a9cc8b0a53fa203557957ca1adc44.patch";
+      hash = "sha256-AYXV2qLHlxuyHC5VqUjDu4qi1TcAl2pMSAi8TEp8db4=";
+    })
+  ];
 
   buildInputs = [
     perl
@@ -54,6 +64,8 @@ stdenv.mkDerivation rec {
   postInstall = ''
     sed -i \
       -e '2i PATH="${pathAdd}:$PATH"' \
+      -e '2i : "''${WINESERVER_BIN:=/run/current-system/sw/bin/wineserver}"' \
+      -e '2i : "''${WINE_BIN:=/run/current-system/sw/bin/.wine}"' \
       "$out/bin/winetricks"
   '';
 
