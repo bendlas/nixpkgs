7c8a93b5ca682f8af971ca6233be3789465062a5
Herwig Hochleitner <herwig@bendlas.net>
2025-09-28 04:00:28 +0200

    bcachefs-tools: 1.31.3 -> 1.31.5

--- pkgs/by-name/bc/bcachefs-tools/package.nix
+++ pkgs/by-name/bc/bcachefs-tools/package.nix
@@ -29,13 +29,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "bcachefs-tools";
-  version = "1.31.3";
+  version = "1.31.5";
 
   src = fetchFromGitHub {
     owner = "koverstreet";
     repo = "bcachefs-tools";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-sXv6YFM91T08WF5dPU7iQNqWbB/QiL2kMaXm6ZtIDqI=";
+    hash = "sha256-5u8GYDmGwQcVfRR67yoCyBi3Vv6kJ5vUPLFjwWlpU9U=";
   };
 
   nativeBuildInputs = [
@@ -66,7 +66,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     src = finalAttrs.src;
-    hash = "sha256-04YrgYfhZ5NfA2BcF2H6Np1SXRiH6CJpkgc9hzlbMAo=";
+    hash = "sha256-d/pDPmxuRObApl+gYEcnh/P34sAIoflztmPHwqZmxfg=";
   };
 
   outputs = [
