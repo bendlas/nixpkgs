7aeda314346255aa6f2648f6f762536c99a58b02
Herwig Hochleitner <herwig@bendlas.net>
2025-10-08 13:38:58 +0200

    libad9361: fix build

--- pkgs/by-name/li/libad9361/package.nix
+++ pkgs/by-name/li/libad9361/package.nix
@@ -19,6 +19,10 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ cmake ];
 
+  cmakeFlags = [
+    "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
+  ];
+
   buildInputs = [ libiio ];
 
   postPatch = lib.optionalString stdenv.hostPlatform.isDarwin ''
