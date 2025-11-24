d61ac14f51021b8fd3e0c8ef1149f4babd9a545e
Herwig Hochleitner <herwig@bendlas.net>
2025-11-24 18:57:06 +0100

    systemd_257: resurrect package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8551,6 +8551,15 @@ with pkgs;
     # break some cyclic dependencies
     util-linux = util-linuxMinimal;
   };
+  systemd_257 = callPackage ../os-specific/linux/systemd-257 {
+    # break some cyclic dependencies
+    util-linux = util-linuxMinimal;
+    # provide a super minimal gnupg used for systemd-machined
+    gnupg = gnupg.override {
+      enableMinimal = true;
+      guiSupport = false;
+    };
+  };
   systemdMinimal = systemd.override {
     pname = "systemd-minimal";
     withAcl = false;
