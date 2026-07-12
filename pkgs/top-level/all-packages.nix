013cc188c95242dff1370c67ac794668d6f5ccf2
Herwig Hochleitner <herwig@bendlas.net>
2026-07-12 18:30:20 +0200

    [WIP] vscodium-from-source: init 1.126.04524

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9540,6 +9540,8 @@ with pkgs;
   vscodium-fhs = vscodium.fhs;
   vscodium-fhsWithPackages = vscodium.fhsWithPackages;
 
+  vscodiumFromSource = callPackage ../applications/editors/vscode/vscodium-from-source.nix { };
+
   antigravity-ide-fhs = antigravity-ide.fhs;
   antigravity-ide-fhsWithPackages = antigravity-ide.fhsWithPackages;
 
