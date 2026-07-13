0a1d1ce598158e0a1a6b4115dbf41be1f074a4d7
Herwig Hochleitner <herwig@bendlas.net>
2026-07-13 15:02:32 +0200

    [WIP] vscodium-from-source: most build

--- pkgs/applications/editors/vscode/vscodium-from-source.nix
+++ pkgs/applications/editors/vscode/vscodium-from-source.nix
@@ -67,7 +67,7 @@ stdenv.mkDerivation (finalAttrs: {
         # nativeBuildInputs = [ git ];
         outputHashMode = "recursive";
         outputHashAlgo = "sha256";
-        outputHash = "sha256-1AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+        outputHash = "sha256-ndV6hqDO0MBYi9DN5wOD06cFIVrL/jO0SKey4tARONo=";
         env = {
           FORCE_EMPTY_CACHE = true;
           FORCE_GIT_DEPS = true;
@@ -80,12 +80,192 @@ stdenv.mkDerivation (finalAttrs: {
         runPhase unpackPhase
         export HOME=$TMPDIR/home
         mkdir $out
-        find -name package-lock.json | ${parallel}/bin/parallel --will-cite -j0 --line-buffer --retries 4 '
-          echo "Prefetching {} ..."
-          ${prefetch-npm-deps}/bin/prefetch-npm-deps "{}" "$out/$(dirname {})"
-          echo "... finished prefetching {}"
-        '
+        for p in $(find -name package-lock.json)
+        do
+          echo "Prefetching $p ..."
+          ${prefetch-npm-deps}/bin/prefetch-npm-deps "$p" "$out/$(dirname $p)"
+          echo "... finished prefetching $p"
+        done
       '';
+
+  env = {
+    # NODE_OPTIONS = "--openssl-legacy-provider";
+    # NODE_ENV = "development";
+
+    # skip unnecessary binary downloads
+    PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = "1";
+    ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
+
+    # ensure the correct node-gyp (from nixpkgs) is used
+    NIX_NODEJS_BUILDNPMPACKAGE = "1";
+    npm_config_nodedir = nodejs;
+    npm_config_node_gyp = "${nodejs}/lib/node_modules/npm/node_modules/node-gyp/bin/node-gyp.js";
+    npm_config_offline = true;
+    npm_config_progress = false;
+
+    # for --fixup-lockfile
+    prefetchNpmDeps = "${prefetch-npm-deps}/bin/prefetch-npm-deps";
+    forceGitDeps = true;
+
+  };
+  nativeBuildInputs = [
+    nodejs
+    nodejs.python
+    pkg-config
+    makeWrapper
+    git
+    jq
+    moreutils
+    openssh
+  ];
+
+  buildInputs =
+    lib.optionals (!stdenv.hostPlatform.isDarwin) [ libsecret ]
+    ++ [
+      libx11
+      libxkbfile
+      libkrb5
+    ]
+    ++ lib.optionals stdenv.hostPlatform.isDarwin [
+      cctools
+    ];
+
+  # akin to `. get-repo.sh`
+  postUnpack = ''
+    cp -R ${finalAttrs.vscodeSrc} $sourceRoot/vscode
+    chmod -R +w $sourceRoot/vscode
+  '';
+
+  preConfigure = ''
+    export HOME=$TMPDIR/home
+    mkdir -p $HOME
+    mkdir -p $TMPDIR
+    cp -R $npmCache $TMPDIR/cache
+    chmod -R +w $TMPDIR/cache
+  '';
+
+  configurePhase = ''
+    runHook preConfigure
+  ''
+  ## unpack all of the prefetched node_modules folders
+  # + ''
+  #   ( cd vscode
+  #     for p in $(find -name package-lock.json -exec dirname {} \;)
+  #     do (
+  #       echo "Setting up $p/node_modules"
+  #       cd $p
+  #       if [ -e node_modules ]
+  #       then
+  #         echo >&2 "File exists $p/node_modules"
+  #         exit 0
+  #       fi
+  #       npm ci --ignore-scripts --cache $TMPDIR/cache/$p
+  #       if [ -e node_modules ]
+  #       then
+  #         patchShebangs node_modules
+  #       else
+  #         echo >&2 "No $p/node_modules, skipping patchShebangs"
+  #       fi
+
+  #     )
+  #     done )
+  # ''
+  + ''
+    ( cd vscode
+      find -name package-lock.json -exec dirname {} \; | ${parallel}/bin/parallel --will-cite --line-buffer '
+        p="{}"
+        echo "Setting up $p/node_modules"
+        cd $p
+        if [ -e node_modules ]
+        then
+          echo >&2 "File exists $p/node_modules"
+          exit 0
+        fi
+        npm ci --ignore-scripts --cache $TMPDIR/cache/$p
+        if [ -e node_modules ]
+        then
+          . $stdenv/setup
+          patchShebangs node_modules
+        else
+          echo >&2 "No $p/node_modules, skipping patchShebangs"
+        fi
+      '
+    )
+  ''
+  ## put ripgrep binary into bin so postinstall does not try to download it
+  + ''
+    find -path "*@vscode/ripgrep" -type d \
+      -execdir mkdir -p {}/bin \; \
+      -execdir ln -s ${ripgrep}/bin/rg {}/bin/rg \;
+  ''
+  ## pre-seed node-gyp
+  + ''
+    mkdir -p $HOME/.node-gyp/${nodejs.version}
+    echo 11 > $HOME/.node-gyp/${nodejs.version}/installVersion
+    ln -sfv ${nodejs}/include $HOME/.node-gyp/${nodejs.version}
+  ''
+  ## node-pty build fix
+  + ''
+    find -path vscode/node_modules/node-pty/scripts/gen-compile-commands.js \
+         -exec substituteInPlace {} \
+                 --replace-fail "npx node-gyp" "$npm_config_node_gyp" \
+               \;
+  ''
+  ## run postinstall scripts
+  + ''
+    find vscode -name package.json -type f | ${parallel}/bin/parallel --will-cite --line-buffer '
+      if jq -e ".scripts.postinstall" {} >-
+      then
+        echo >&2 "Running postinstall script in $(dirname {})"
+        npm --prefix=$(dirname {}) run postinstall
+      fi
+      exit 0
+    '
+  ''
+  # ## rebuild native binaries
+  # + ''
+  #   echo >&2 "Rebuilding from source in ./remote"
+  #   npm --prefix vscode/remote rebuild --build-from-source
+  # ''
+  + ''
+    runHook postConfigure
+  '';
+
+  buildPhase = ''
+    runHook preBuild
+
+    export SHOULD_BUILD="yes"
+    export SHOULD_BUILD_REH="yes"
+    export SHOULD_BUILD_REH_WEB="yes"
+    export CI_BUILD="no"
+    export OS_NAME="linux"
+    export VSCODE_ARCH="${vsBuildTarget}"
+    export VSCODE_QUALITY="stable"
+    export RELEASE_VERSION="${finalAttrs.version}"
+
+    . build.sh    
+
+    runHook postBuild
+  '';
+
+  # buildPhase = ''
+  #   runHook preBuild
+
+  #   npm run gulp vscode-reh-web-${vsBuildTarget}-min
+
+  #   runHook postBuild
+  # '';
+
+  # installPhase = ''
+  #   runHook preInstall
+
+  #   mkdir -p $out
+  #   cp -R -T ../vscode-reh-web-${vsBuildTarget} $out
+  #   ln -sf ${nodejs}/bin/node $out
+
+  #   runHook postInstall
+  # '';
+
 
 })
 
