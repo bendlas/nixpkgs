18f574948de98f02761d790fb22e8202c6fcdc2f
Herwig Hochleitner <herwig@bendlas.net>
2026-07-13 14:59:18 +0200

    [SLOP] npm-cache-prefetch-reentrant

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2431,6 +2431,8 @@ with pkgs;
     prefetch-npm-deps
     ;
 
+  fetch-npm-deps-reentrant = callPackages ../build-support/node/prefetch-npm-deps-reentrant { };
+    
   importNpmLock = callPackages ../build-support/node/import-npm-lock { };
 
   ldapdomaindump = with python3Packages; toPythonApplication ldapdomaindump;
